031ff4269b3fbf1638afdaa15dd3608d0dcd1300
Ronald <ronald1das@gmail.com>
2020-03-28 17:27:30 +0530

    Added postgres db table and storing each prediction

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 import requests
 import numpy as np
 import cv2
 from scripts.inference import Evaluator
+# import time
+
 from db import write_output_to_db
-model=Evaluator("model/COVID-Netv1/")
-app=Flask(__name__)
-@app.route("/predict",methods=['GET'])
+MODEL_VERSION='v0.1'
+model = Evaluator("model/COVID-Netv1/")
+app = Flask(__name__)
+
+
+@app.route("/predict", methods=['GET'])
 def predict():
-    image_loc=request.args['image_loc']
-    img_resp=requests.get(image_loc,stream=True).raw
+    image_loc = request.args.get('image_loc')
+    img_resp = requests.get(image_loc, stream=True).raw
     image = np.asarray(bytearray(img_resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    resp=model.evaluate(image)
-    return jsonify(resp)
+    resp = model.evaluate(image)
+    write_output_to_db({'img_url':image_loc,'model_version':MODEL_VERSION,'model_output':resp})
+    return jsonify({'result': resp})
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
--- db.py
+++ db.py
@@ -1,7 +1,39 @@
 import os
 import psycopg2
-# cnx_string=os.getenv('postgre')
-conn = psycopg2.connect(host="localhost",database="suppliers", user="postgres", password="postgres")
 
-def write_output_to_db():
-    pass
+postgres_host = os.getenv('POSTGRES_HOST')
+postgres_db = os.getenv('POSTGRES_DB')
+postgres_user = os.getenv('POSTGRES_USER')
+postgres_password = os.getenv('POSTGRES_PASSWORD')
+conn = psycopg2.connect(host=postgres_host, database=postgres_db, user=postgres_user, password=postgres_password)
+
+
+def write_output_to_db(params_dict):
+    cursor = conn.cursor()
+    query = "INSERT INTO xray_results(img_url,model_version,model_output) VALUES (%(img_url)s,%(model_version)s,%(model_output)s)"
+    cursor.execute(query, params_dict)
+    conn.commit()
+    cursor.close()
+
+
+def setup_db():
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS xray_results (
+    id serial primary key ,
+    img_url text not null ,
+    model_version varchar (50) not null ,
+    model_output varchar(100) not null,
+    gender varchar(10),
+    age int,
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    user_id int
+);
+    """)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    setup_db()
