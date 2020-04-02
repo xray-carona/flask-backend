bf10195af65aa1b424abddeb7bfedd120142ad19
Ronald <ronald1das@gmail.com>
2020-04-02 19:22:53 +0530

    exporting tensorflow model, keeping it in memory

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import requests
 import numpy as np
 import cv2
 from scripts.inference import Evaluator
-# import time
+import time
 
 from db import write_output_to_db
 MODEL_VERSION='v0.1'
 model = Evaluator("model/COVID-Netv1/")
+sess,x,op_to_restore=model.export()
 app = Flask(__name__)
 
 @app.route("/",methods=["GET"])
@@ -16,12 +17,17 @@ def main():
 
 @app.route("/predict", methods=['GET'])
 def predict():
+    # a=time.time()
     image_loc = request.args.get('image_loc')
     img_resp = requests.get(image_loc, stream=True).raw
     image = np.asarray(bytearray(img_resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    resp = model.evaluate(image)
+    resp=model.predict(image,sess,x,op_to_restore)
+
+    # resp = model.evaluate(image)
     write_output_to_db({'img_url':image_loc,'model_version':MODEL_VERSION,'model_output':resp})
+    # b = time.time()
+    # print(b - a)
     return jsonify({'result': resp})
 
 
